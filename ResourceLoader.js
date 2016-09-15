e04d7cc2ced0c61e3e47980b790e2a0f096e8cf4
moczka <steven.guesp@gmail.com>
2016-09-15 19:41:07 -0400

    Uploaded core files for the demo and module lib

---
+++ ResourceLoader.js
@@ -0,0 +1,119 @@
+var ResourceLoader = (function(){
+	
+	var totalAssets = 0,
+		loadedAssets = 0,
+		UserOptions,
+		percentageLoaded,
+		audioSupport,
+		assets = {};
+	
+		//default options if no option argument is passed by user.
+		var defaultOptions = {
+					  onload: function(){ 
+						console.log('No callback was passed in..');
+						},
+					  final : function(){
+						  console.log('No final function to call was passed in..');
+					  },
+					 assets : []
+					 };
+	
+	/* Example of options:
+	
+	{
+		assets : {
+		
+			imgs : {
+				//img names with URLs
+				bg_black : "assets/img/background.png,
+				meteorSprite : "assets/img/meteor.png,
+				enemiesSprite : "assets/img/enemies.png,
+				enemyShipSprites: "assets/img/enemyShips.png
+			},
+			sounds : {
+				//sounds name with URLs
+				soundtrack : ["assets/sounds/player.mp3", "assets/sounds/player.wav"],
+				endSound : ["assets/sounds/gameover.mp3", "assets/sounds/gameover.wav"]
+			}
+		}
+			//function that is called on every item loaded
+		onload : updateLoaderDisplayer,
+			//function that is called once all have loaded
+		final : finalFunctionToCall
+	}
+
+	*/
+		
+	function init(options){
+		UserOptions = options || defaultOptions;
+	}
+	
+	function download(){
+		//return if all assets have already loaded and indexed.
+		if(percentageLoaded === 1) return;
+		//creates the sound and image elements for each asset in the options argument.
+		for(var item in UserOptions.assets.imgs){
+			assets[item] = new Image();
+			assets[item].src = UserOptions.assets.imgs[item];
+			assets[item].addEventListener('load', onLoad, false);
+			totalAssets++;
+		}
+		//arrays are passed for each sound asset containing the source to the sound.
+		for(var item in UserOptions.assets.sounds){
+			for(var i=0, j=UserOptions.assets.sounds[item].length; i<j; i++){
+			var currentSound = UserOptions.assets.sounds[item][i],
+				//analyzes the extension and picks the first one that is supported by the browser.
+			currentSoundExtension = currentSound.slice(currentSound.length-3, currentSound.length);
+				if(isAudioSupport(currentSoundExtension)){
+					assets[item] = new Audio();
+					assets[item].src = UserOptions.assets.sounds[item][i];
+					assets[item].addEventListener('canplaythrough', onLoad, false);
+					totalAssets++;
+					break;
+				}
+			}
+
+		}
+		
+	}
+	
+	function onLoad(item){
+		//increments load count and remove listeners.
+		loadedAssets++;
+		if(item.target.tagName === "AUDIO"){
+			item.target.removeEventListener('canplaythrough', onLoad, false);
+		}else{
+			item.target.removeEventListener('load', onLoad, false);
+		}
+		
+		var itemLoaded = item.target;
+		percentageLoaded = Math.floor((loadedAssets / totalAssets)*100)/100;
+		
+    	if(UserOptions.onload) UserOptions.onload(itemLoaded);
+		
+		if(loadedAssets >= totalAssets && UserOptions.final){
+			UserOptions.final();
+		}
+	}
+	
+	function isAudioSupport(extension){
+		
+		var audio = new Audio();
+		var supportValue = audio.canPlayType("audio/"+extension);
+		if( supportValue === "maybe" || supportValue === "probably" ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	return {
+		init : init,
+		assets : assets,
+		get loaded(){
+			return percentageLoaded;
+		},
+    	downloadAll : download
+	}
+	
+})();
---
+++ main.js
@@ -0,0 +1,42 @@
+window.onload = function(){
+	initApp();
+}
+function initApp(){
+	var statusBar = document.getElementById('statusBar'),
+		loadButton = document.getElementById('loadAssets'),
+		maxWidth = 300;
+
+	var loaderOptions = {
+		assets : {
+			imgs : {
+				bg : "assets/sprites/background.png",
+				enemy_ships : "assets/sprites/enemyShips.png",
+				meteorSprite : "assets/sprites/meteorSprite.png",
+				mothershipSprite : "assets/sprites/motherships.png"
+			},
+			sounds : {
+				shoot: ["assets/sounds/shoot.wav", "assets/sounds/shoot.mp3"],
+				soundTrack : ["assets/sounds/soundtrack.wav", "assets/sounds/soundtrack.mp3"],
+				perk : ["assets/sounds/perk.mp3", "assets/sounds/perk.wav"],
+				victory : ["assets/sounds/victory.wav", "assets/sounds/victory.mp3"]
+			}
+		},
+		onload : updateStatus,
+		final : finalCall
+	}
+	
+	ResourceLoader.init(loaderOptions);
+	
+	loadButton.onclick = function(){
+		ResourceLoader.downloadAll();
+	};
+	
+	function updateStatus(){
+		var currentWidth = Math.floor(maxWidth*ResourceLoader.loaded);
+		statusBar.setAttribute('style', 'width: '+currentWidth+'px;');
+		console.log('1 asset has loaded');
+	}
+	function finalCall(){
+		console.log('all assets have loaded');
+	}
+}
